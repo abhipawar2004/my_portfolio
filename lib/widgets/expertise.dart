e6893d0c307d805961d66290b7fa3b5391cab267
Abhishek <pawarabhi2004@gmail.com>
2024-10-15 20:58:39 +0530

    Refactor expertise screen and add design and phone images

--- lib/widgets/expertise.dart
+++ lib/widgets/expertise.dart
@@ -5,12 +5,64 @@ class ExpertiseScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-     return Column(children: [
-      Text.rich(TextSpan(children: [
-        TextSpan(text: 'I like to make things easy and fun',
-        style: TextStyle()),
-      ],),),
-     ],
+    return Column(
+      children: [
+        Text.rich(
+          TextSpan(
+            children: [
+              TextSpan(
+                text: 'I like to make things easy and fun\n\n',
+                style: TextStyle(color: Color(0xffFFFFFF), fontSize: 16),
+              ),
+              
+              TextSpan(
+                text: 'My ',
+                style: TextStyle(color: Color(0xffFFFFFF), fontSize: 34),
+              ),
+              TextSpan(
+                text: 'Special Services ',
+                style: TextStyle(color: Color(0xffFF014F), fontSize: 34),
+              ),
+              TextSpan(
+                text: 'for your\n' 'Business ',
+                style: TextStyle(color: Color(0xffFFFFFF), fontSize: 34),
+              ),
+              TextSpan(
+                text: 'Development\n\n',
+                style: TextStyle(color: Color(0xffFF014F), fontSize: 34),
+              ),
+            ],
+          ),
+          textAlign: TextAlign.center,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              height:300,
+              width: 300,
+              decoration: BoxDecoration(
+                  color: Color(0xff1E1E1E),
+                  borderRadius: BorderRadius.all(Radius.circular(10),),),
+                  child: Column(
+                    
+                    children: [
+                        Image.asset('assets/images/Phone.png',
+                        height: 28,)
+                    ],
+                  ),
+            ),
+             Container(
+              height:280,
+              width: 280,
+              decoration: BoxDecoration(
+                  color: Color(0xff1E1E1E),
+                  borderRadius: BorderRadius.all(Radius.circular(10))),
+            ),
+            
+          ],
+        ),
+      ],
     );
   }
 }
